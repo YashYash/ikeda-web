58533a89b077225d7a10053b3a17cea1a030a8e0
Yash <yash.saxena1217@gmail.com>
2015-03-12 23:43:02 -0700

    fixed spelling mistake

--- public/javascripts/controllers/landingController.js
+++ public/javascripts/controllers/landingController.js
@@ -1,54 +1,55 @@
 ikeda.controller('LandingController', [
-	'$scope', 
-	'$state',
-	'$rootScope',
-	function(
-		$scope,
-		$state,
-		$rootScope) {
-	'use strict';
-	console.log('#### Landing Controller');
-	$scope.showBackground = 'one';
-	$scope.loadingSuccess = 'false';
-	$scope.applyBackground = function(number) {
-		$scope.showBackground = number;
-	};
+  '$scope',
+  '$state',
+  '$rootScope',
+  function(
+    $scope,
+    $state,
+    $rootScope) {
+    'use strict';
+    console.log('#### Landing Controller');
+    $scope.showBackground = 'one';
+    $scope.loadingSuccess = 'false';
+    $scope.applyBackground = function(number) {
+      $scope.showBackground = number;
+    };
 
-	$scope.imageLoaded = function() {
-		console.log('####### Image has loaded ########');
-		setTimeout(function() {
-			$scope.loadingSuccess = 'true';
-			$scope.showNameAndIcons();
-			$scope.$apply();
-		}, 3000);
-		setTimeout(function() {
-			$rootScope.allImagesHaveLoaded = 'true';
-		}, 3500);
-	};	
-	$scope.showNameAndIcons = function() {
-		setTimeout(function() {
-			$scope.showName = 'true';
-		}, 500);
-		setTimeout(function() {
-			$scope.showMusic = 'true';
-		}, 1000);	
-		setTimeout(function() {
-			$scope.showSocial = 'true';
-		}, 1500);
-		setTimeout(function() {
-			$scope.showShop = 'true';
-		}, 2000);
-		setTimeout(function() {
-			$scope.showBooking = 'true';
-		}, 2500);							
-	}
+    $scope.imageLoaded = function() {
+      console.log('####### Image has loaded ########');
+      setTimeout(function() {
+        $scope.loadingSuccess = 'true';
+        $scope.showNameAndIcons();
+        $scope.$apply();
+      }, 3000);
+      setTimeout(function() {
+        $rootScope.allImagesHaveLoaded = 'true';
+      }, 3500);
+    };
+    $scope.showNameAndIcons = function() {
+      setTimeout(function() {
+        $scope.showName = 'true';
+      }, 500);
+      setTimeout(function() {
+        $scope.showMusic = 'true';
+      }, 1000);
+      setTimeout(function() {
+        $scope.showSocial = 'true';
+      }, 1500);
+      setTimeout(function() {
+        $scope.showShop = 'true';
+      }, 2000);
+      setTimeout(function() {
+        $scope.showBooking = 'true';
+      }, 2500);
+    }
 
-	$scope.goToDesktopRoute = function(route) {
-		if(route === 'music') {
-			$state.go('app.v1.music-desktop');
-		}
-		if(route === 'booking') {
-			$state.go('app.v1.booking-desktop');
-		}
-	}
-}]);
+    $scope.goToDesktopRoute = function(route) {
+      if (route === 'music') {
+        $state.go('app.v1.music-desktop');
+      }
+      if (route === 'booking') {
+        $state.go('app.v1.booking-desktop');
+      }
+    }
+  }
+]);
