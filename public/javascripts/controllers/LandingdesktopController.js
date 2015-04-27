06b5b28e5cd7d15c93cdc3141a62000b5e777c73
Yash <yash.saxena1217@gmail.com>
2015-04-27 00:21:10 -0700

    trying again

---
+++ public/javascripts/controllers/LandingdesktopController.js
@@ -0,0 +1,82 @@
+ikeda.controller('LandingdesktopController', [
+  '$scope',
+  '$state',
+  '$rootScope',
+  '$timeout',
+  function(
+    $scope,
+    $state,
+    $rootScope,
+    $timeout) {
+    'use strict';
+    console.log('#### Landing Controller');
+
+    // Init
+    var init = function() {
+      var backgroundVideo = document.getElementById('background-video');
+      backgroundVideo.play();
+      backgroundVideo.addEventListener('loadedmetadata', function() {
+        var video = this;
+        var startVideo = function() {
+          console.log('#### Starting the video');
+          video.currentTime = 35;
+          $timeout(function() {
+            video.currentTime = 70;
+            $timeout(function() {
+              video.currentTime = 95;
+              $timeout(function() {
+                startVideo();
+              }, 7000);
+            }, 7000);
+          }, 7000);
+        };
+        startVideo();
+      }, false);
+
+      $timeout(function() {
+        $scope.dimensions = $rootScope.getDimensions();
+        $scope.startLandingAnimations();
+      }, 500);
+
+      // Ui-repsonders
+      $scope.hoverLandingLinks = function(route) {
+        $scope.currentHoverLink = route;
+      };
+      $scope.startLandingAnimations = function() {
+        $timeout(function() {
+          $scope.showLandingPlayButton = true;
+          $timeout(function() {
+            $scope.showIkedaName = true;
+            $timeout(function() {
+              $scope.showLandingCircle = true;
+              $timeout(function() {
+                $scope.showLandingLines = true;
+                $timeout(function() {
+                  $scope.showLandingLink = 1;
+                  $timeout(function() {
+                    $scope.showLandingLink = 2;
+                    $timeout(function() {
+                      $scope.showLandingLink = 3;
+                    }, 500);
+                  }, 500);
+                }, 500);
+              }, 500);
+            }, 500);
+          }, 1000);
+        }, 1000);
+      };
+
+      // Ui-relayers
+      $rootScope.$on('window resized', function(e) {
+        console.log('#### Window has been resized');
+        e.preventDefault();
+        $scope.$apply(function() {
+          $scope.dimensions = $rootScope.getDimensions();
+        });
+      });
+    };
+    $timeout(function() {
+      init();
+    }, 1000);
+  }
+]);
